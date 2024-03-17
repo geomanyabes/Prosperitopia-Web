9030f623da968ce11266c0855a09ae5d217c760e
Geoman Yabes <geoman.yabes@gmail.com>
2024-03-17 20:20:36 +0800

    components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: 'items', component: ItemListComponent },
+  { path: 'items/:id', component: ItemDetailsComponent },
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'categories/:id', component: CategoryDetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
